2e86c983600f7d22b18f63ebf46f4bbb2831b875
DengSongsong <DengSongsong1010@163.com>
2018-02-01 16:00:55 +0800

    项目资源准备

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -10,6 +10,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
+const appData = require('../data.json')
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
+
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
 
@@ -22,6 +27,27 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
   // these devServer options should be customized in /config/index.js
   devServer: {
+    before(app){
+      app.get('/api/seller', function(req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      });
+      app.get('/api/goods', function(req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      });
+      app.get('/api/ratings', function(req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      });
+
+    },
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
